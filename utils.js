8272c61823053199d3a632bf0ce64a6d6d4f548a
Josh Marinacci <joshua@marinacci.org>
2019-12-23 15:12:28 -0800

    initial import

--- utils.js
+++ utils.js
@@ -1,2 +1,7 @@
 export const randRange = (min,max) => min + Math.random()*(max-min)
 export const hsl = (h, s, l) => `hsl(${h*360},${s*100}%,${l*100}%)`
+
+
+export function circle(c, pt, radius) {
+    c.arc(pt.x,pt.y,radius,0,Math.PI*2)
+}
